d5fae8891e9c558f808bca33304f239cf3d26ca2
Moe Irikida <irimodi@gmail.com>
2021-11-08 18:43:48 +0900

    クリックしていくと切り抜かれるものを実装するため、ダミー画像を作成

--- web/app.py
+++ web/app.py
@@ -1,6 +1,6 @@
 #coding: utf-8
 from flask import Flask, render_template
-# import cv2
+import cv2
 
 app = Flask(__name__, static_folder='./resource')
 
@@ -15,6 +15,11 @@ def page_all():
 @app.route('/several.html')
 def page_several():
     return render_template("several.html")
+
+@app.route('/crop')
+def page_crop():
+    # crop 処理をした後、処理後画像を返す
+    return "/work/dummy.jpg"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
