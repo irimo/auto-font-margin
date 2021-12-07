0f0a5404b3e3f52ab1a1a0a26175a1a53fa299e0
Moe Irikida <irimodi@gmail.com>
2021-12-07 19:34:51 +0900

    画像たくさん出力用 dockerfile できた

--- padding/pad.py
+++ padding/pad.py
@@ -1,4 +1,14 @@
 #coding: utf-8
 
-import cv2
-import numpy as np
+from ttfquery import describe
+from ttfquery import glyphquery
+import ttfquery.glyph as glyph
+
+char = "a"
+font_url = ./dist/ttfs/VL-Gothic-Regular.ttf"
+font = describe.openFont(font_url)
+g = glyph.Glyph(char)
+contours = g.calculateContours(font)
+for contour in contours:
+    for point, flag in contour:
+        print point, flag
